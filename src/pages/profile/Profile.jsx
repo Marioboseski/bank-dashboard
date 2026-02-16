329a130fc32792c87d06cd900f4d10fb0e2d2d2c
Mario Boseski <crboseskimario@gmail.com>
2026-02-16 16:24:00 +0100

    feat(profile): implement user context, balance display, logout and add styles

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,14 +1,21 @@
 import { UserContext } from "../../context/UserContext";
 import { useContext } from "react";
+import { FinanceContext } from "../../context/FinanceContext";
 import { useNavigate } from "react-router-dom";
+import { CircleOff } from "lucide-react";
 
 const Profile = () => {
-  
+
   const { user, logoutUser } = useContext(UserContext);
   const navigate = useNavigate();
 
-  if(!user) {
-    return <p>No user logged in</p>
+  if (!user) {
+    return (
+      <div className="flex flex-col justify-center items-center gap-2 min-h-dvh">
+        <CircleOff className="w-full max-w-32 min-h-32"/>
+        <p>No user logged in</p>
+      </div>
+    )
   }
 
   const handleLogout = () => {
@@ -16,11 +23,27 @@ const Profile = () => {
     navigate("/")
   }
 
+  const { balance } = useContext(FinanceContext);
+  const firstLetter = user.name?.charAt(0).toUpperCase();
+
+  const formattedBalance = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "EUR",
+  }).format(balance);
+
   return (
-    <div>
-      <p>{user?.name}</p>
-      <p>{user?.email}</p>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="flex justify-center items-center min-h-dvh p-3">
+      <div className="flex flex-col justify-center items-center w-full gap-3 p-3 border-2 border-gray-300 rounded-xl shadow-lg">
+        <p className="flex justify-center items-center bg-blue-300 border-2 border-blue-600 rounded-full w-full max-w-36 min-h-36 text-3xl">{firstLetter}</p>
+        <p className="font-semibold text-2xl">{user?.name}</p>
+        <p className="text-lg">{user?.email}</p>
+        <div className="flex flex-col justify-center items-center gap-3 border-2 border-gray-300 rounded-lg w-full max-w-64 min-h-40">
+          <p className="text-xl text-gray-500">Current balance</p>
+          <p className="text-xl border-b-2 border-gray-300 p-1">{formattedBalance}</p>
+          <button className="bg-blue-300 border-2 border-blue-400 text-lg text-blue-600 rounded-md  p-1 w-full max-w-40">Edit profile</button>
+        </div>
+        <button onClick={handleLogout} className="bg-gray-300 border-2 border-black/80 rounded-md text-lg p-1 w-full max-w-24">Logout</button>
+      </div>
     </div>
   );
 }
