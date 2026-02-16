8a53edb4811b5ccc51a4b586f3b6c378f9f13e34
Mario Boseski <crboseskimario@gmail.com>
2026-02-16 16:25:38 +0100

    Implement user from UserContext and format balance

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { FinanceContext } from "../../context/FinanceContext";
+import { UserContext } from "../../context/UserContext";
 import TransactionsItem from "../../components/TransactionsItem";
 
 const Dashboard = () => {
 
   const { balance, transactions } = useContext(FinanceContext);
-
+  const { user } = useContext(UserContext);
   const recentTransactions = transactions.slice(0, 5);
+
+  const formattedBalance = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "EUR",
+  }).format(balance);
 
   return (
     <div className="flex flex-col justify-center items-center p-3 gap-3">
-      <h3 className="text-3xl">Welcome back, USER</h3>
+      <h3 className="text-3xl">Welcome back, {user?.name}</h3>
       <p className="text-gray-400">Here is your financial overview</p>
       <div className="flex flex-col justify-center items-center gap-3 border-2 border-gray-300 rounded-md w-full p-2">
         <p className="text-xl text-gray-400">Your current balance</p>
-        <p className="text-2xl">BALANCE {balance}</p>
+        <p className="text-2xl">BALANCE {formattedBalance}</p>
         <div className="flex p-2">
           <Link to={"/addmoney"} className="bg-blue-600 p-3 rounded-l-md text-xl">Add money</Link>
           <Link to={"/paybill"} className="bg-blue-500 p-3 rounded-r-md text-xl">Pay bill</Link>
